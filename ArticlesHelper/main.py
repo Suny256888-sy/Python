4d9ef5eaf437ee433dd7589865da341119d9a45e
evilbutcher <pgcrfhht@outlook.com>
2020-08-14 22:18:57 +0800

    update demo: add libgen download link

--- ArticlesHelper/main.py
+++ ArticlesHelper/main.py
@@ -16,53 +16,99 @@ def init():
 
 
 def downloadfile(name, doi, url):
-    pdf = requests.get(url)
-    # 保存文件
-    if pdf.status_code == 200:
-        with open('ArticlesHelper/articles/' + name, 'wb') as f:
-            f.write(pdf.content)
-        table = Table(show_header=True, header_style="bold magenta")
-        table.add_column("文件名")
-        table.add_column("状态")
-        table.add_row(name, '[bold green]下载完成[/bold green]')
-        console.print(table)
-    else:
-        table = Table(show_header=True, header_style="bold magenta")
-        table.add_column("文件名")
-        table.add_column("状态")
-        table.add_row(doi, '[bold red]下载失败[/bold red]')
-        console.print(table)
-        print(pdf)
-
-
-def stwgetdllink(link):
-    data = requests.get(link)
-    # 获取下载链接
-    html = data.text
-    afterhtml = BeautifulSoup(html, 'html.parser')
-    div = afterhtml.find('iframe', id='pdf')
-    if div is None:
-        print('[bold red]未找到文献[/bold red]')
-    else:
-        pdflink = div.get('src')
-        # print(re.match(r'https:', pdflink))
-        if re.match(r'https:', pdflink) is None:
-            postlink = 'https:' + pdflink
+    try:
+        pdf = requests.get(url)
+        # 保存文件
+        if pdf.status_code == 200:
+            with open('ArticlesHelper/articles/' + name, 'wb') as f:
+                f.write(pdf.content)
+            table = Table(show_header=True, header_style="bold magenta")
+            table.add_column("文件名")
+            table.add_column("状态")
+            table.add_row(name, '[bold green]下载完成[/bold green]')
+            console.print(table)
         else:
-            postlink = pdflink
-    print('下载链接：' + postlink)
-    return postlink
+            table = Table(show_header=True, header_style="bold magenta")
+            table.add_column("文件名")
+            table.add_column("状态")
+            table.add_row(doi, '[bold red]下载失败[/bold red]')
+            console.print(table)
+            print(pdf)
+    except:
+        table = Table(show_header=True, header_style="bold magenta")
+        table.add_column("文件名")
+        table.add_column("状态")
+        table.add_row(name, '[bold red]下载文件出错啦[/bold red]')
+        console.print(table)
+
+
+def stwgetdllink(name):
+    try:
+        hostlink = 'https://sci-hub.tw/'
+        link = hostlink + doi
+        print('请求链接：' + link)
+        data = requests.get(link)
+        # 获取下载链接
+        html = data.text
+        afterhtml = BeautifulSoup(html, 'html.parser')
+        div = afterhtml.find('iframe', id='pdf')
+        if div is None:
+            table = Table(show_header=True, header_style="bold magenta")
+            table.add_column("文件名")
+            table.add_column("状态")
+            table.add_row(name, '[bold red]未找到文献[/bold red]')
+            console.print(table)
+        else:
+            pdflink = div.get('src')
+            # print(re.match(r'https:', pdflink))
+            if re.match(r'https:', pdflink) is None:
+                postlink = 'https:' + pdflink
+            else:
+                postlink = pdflink
+        print('下载链接：' + postlink)
+        return postlink
+    except:
+        table = Table(show_header=True, header_style="bold magenta")
+        table.add_column("文件名")
+        table.add_column("状态")
+        table.add_row(name, '[bold red]获取链接出错啦[/bold red]')
+        console.print(table)
+
+
+def lbggetdllink(name):
+    try:
+        hostlink = 'https://libgen.lc/scimag/ads.php?doi='
+        link = hostlink + doi
+        print('请求链接：' + link)
+        data = requests.get(link)
+        # 获取下载链接
+        html = data.text
+        afterhtml = BeautifulSoup(html, 'html.parser')
+        div = afterhtml.find('a')
+        if div is None:
+            table = Table(show_header=True, header_style="bold magenta")
+            table.add_column("文件名")
+            table.add_column("状态")
+            table.add_row(name, '[bold red]未找到文献[/bold red]')
+            console.print(table)
+        else:
+            pdflink = div.get('href')
+        print('下载链接：' + pdflink)
+        return pdflink
+    except:
+        table = Table(show_header=True, header_style="bold magenta")
+        table.add_column("文件名")
+        table.add_column("状态")
+        table.add_row(name, '[bold red]获取链接出错啦[/bold red]')
+        console.print(table)
 
 
 if __name__ == "__main__":
     init()
     console = Console()
     doi = input('请输入DOI：')
-    hostlink = 'https://sci-hub.tw/'
-    link = hostlink + doi
     filename = doi.replace('/', '_') + '.pdf'
     print('DOI：' + doi)
     print('保存文件名：' + filename)
-    print('请求链接：' + link)
-    downloadfile(filename, doi, stwgetdllink(link))
-    input('按任意键退出')
+    downloadfile(filename, doi, lbggetdllink(filename))
+input('按任意键退出')
