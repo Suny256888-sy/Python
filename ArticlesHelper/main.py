0e8e927e58a0120da9df41b75058684bb17491b0
evilbutcher <pgcrfhht@outlook.com>
2020-08-14 17:21:34 +0800

    rename

--- ArticlesHelper/main.py
+++ ArticlesHelper/main.py
@@ -1,5 +1,6 @@
 import requests
 import os
+# import re
 from pathlib import Path
 from bs4 import BeautifulSoup
 
@@ -8,19 +9,24 @@ if __name__ == "__main__":
     hostlink = 'https://sci-hub.tw/'
     link = hostlink + doi
     filename = doi.replace('/', '_') + ".pdf"
+    print(filename)
     # 判断目录
     path = Path('ArticlesHelper/articles/')
-    exist = path.is_dir()
-    if exist == False:
+    if path.is_dir() is False:
         os.mkdir(path)
-    print(link)
     # 下载文件
+    print(link)
     data = requests.get(url=link)
     html = data.text
     afterhtml = BeautifulSoup(html, 'html.parser')
-    pdflink = afterhtml.find('iframe', id='pdf').get("src")
-    pdf = requests.get(pdflink)
-    # 保存文件
-    with open('ArticlesHelper/articles/' + filename, 'wb') as f:
-        f.write(pdf.content)
+    div = afterhtml.find('iframe', id='pdf')
+    if div is None:
+        print('未找到文献')
+    else:
+        pdflink = div.get("src")
+        pdf = requests.get(pdflink)
+        # 保存文件
+        with open('ArticlesHelper/articles/' + filename, 'wb') as f:
+            f.write(pdf.content)
         f.close()
+        print(filename + ' 下载完成')
