f09e30aceb5631f0fea61448f14b78c299667eb0
evilbutcher <pgcrfhht@outlook.com>
2020-08-16 00:54:44 +0800

    [add] better progress bar

--- ArticlesHelper/main.py
+++ ArticlesHelper/main.py
@@ -6,36 +6,60 @@ from rich import print
 from rich.table import Table
 from pathlib import Path
 from bs4 import BeautifulSoup
+
+from concurrent.futures import ThreadPoolExecutor
+from functools import partial
 from urllib.request import urlopen
-from tqdm import tqdm
+
+from rich.progress import (
+    BarColumn,
+    DownloadColumn,
+    TextColumn,
+    TransferSpeedColumn,
+    TimeRemainingColumn,
+    Progress,
+    TaskID,
+)
+
+progress = Progress(
+    TextColumn("[bold blue]{task.fields[filename]}", justify="right"),
+    BarColumn(bar_width=None),
+    "[progress.percentage]{task.percentage:>3.1f}%",
+    "•",
+    DownloadColumn(),
+    "•",
+    TransferSpeedColumn(),
+    "•",
+    TimeRemainingColumn(),
+)
 
 
-def download_from_url(url, dst):
-    """
-    @param: url to download file
-    @param: dst place to put the file
-    """
-    file_size = int(urlopen(url).info().get('Content-Length', -1))
-    if os.path.exists(dst):
-        first_byte = os.path.getsize(dst)
-    else:
-        first_byte = 0
-    if first_byte >= file_size:
-        return file_size
-    header = {"Range": "bytes=%s-%s" % (first_byte, file_size)}
-    pbar = tqdm(total=file_size,
-                initial=first_byte,
-                unit='B',
-                unit_scale=True,
-                desc=url.split('/')[-1])
-    req = requests.get(url, headers=header, stream=True)
-    with (open(dst, 'ab')) as f:
-        for chunk in req.iter_content(chunk_size=1024):
-            if chunk:
-                f.write(chunk)
-                pbar.update(1024)
-    pbar.close()
-    return file_size
+def copy_url(task_id: TaskID, url: str, path: str) -> None:
+    """Copy data from a url to a local file."""
+    response = urlopen(url)
+    # This will break if the response doesn't contain content length
+    progress.update(task_id, total=int(response.info()["Content-length"]))
+    with open(path, "wb") as dest_file:
+        progress.start_task(task_id)
+        for data in iter(partial(response.read, 32768), b""):
+            dest_file.write(data)
+            progress.update(task_id, advance=len(data))
+
+
+def download(urls, dest_dir):
+    """Download multuple files to the given directory."""
+    with progress:
+        with ThreadPoolExecutor(max_workers=4) as pool:
+            try:
+                for key, value in urls.items():
+                    filename = key.replace('/', '_') + '.pdf'
+                    dest_path = os.path.join(dest_dir, filename)
+                    task_id = progress.add_task("download",
+                                                filename=filename,
+                                                start=False)
+                    pool.submit(copy_url, task_id, value, dest_path)
+            except (Exception):
+                print(key + value + '出错啦')
 
 
 def init():
@@ -43,27 +67,6 @@ def init():
     path = Path('./articles/')
     if path.is_dir() is False:
         os.mkdir(path)
-
-
-def downloadfile(name, doi, url):
-    if url is None:
-        return False
-    try:
-        # 保存文件
-        download_from_url(url, "./articles/" + name)
-        table = Table(show_header=True, header_style="bold magenta")
-        table.add_column("文件名")
-        table.add_column("状态")
-        table.add_row(name, '[bold green]下载完成[/bold green]')
-        console.print(table)
-        return True
-    except (Exception):
-        table = Table(show_header=True, header_style="bold magenta")
-        table.add_column("文件名")
-        table.add_column("状态")
-        table.add_row(name, '[bold red]下载文件出错啦[/bold red]')
-        console.print(table)
-        return False
 
 
 def stwgetdllink(name, doi):
@@ -76,20 +79,21 @@ def stwgetdllink(name, doi):
         afterhtml = BeautifulSoup(html, 'html.parser')
         div = afterhtml.find('iframe', id='pdf')
         pdflink = div.get('src')
-        # print(re.match(r'https:', pdflink))
         if re.match(r'https:', pdflink) is None:
             postlink = 'https:' + pdflink
         else:
             postlink = pdflink
         print('下载链接：' + postlink)
-        return postlink
+        urls[doi] = postlink
+        return True
     except (Exception):
         table = Table(show_header=True, header_style="bold magenta")
         table.add_column("文件名")
         table.add_column("状态")
         table.add_row(name, '[bold red]未找到文献[/bold red]')
         console.print(table)
-        return None
+        urls[doi] = None
+        return False
 
 
 def ssegetdllink(name, doi):
@@ -102,20 +106,21 @@ def ssegetdllink(name, doi):
         afterhtml = BeautifulSoup(html, 'html.parser')
         div = afterhtml.find('iframe', id='pdf')
         pdflink = div.get('src')
-        # print(re.match(r'https:', pdflink))
         if re.match(r'https:', pdflink) is None:
             postlink = 'https:' + pdflink
         else:
             postlink = pdflink
         print('下载链接：' + postlink)
-        return postlink
+        urls[doi] = postlink
+        return True
     except (Exception):
         table = Table(show_header=True, header_style="bold magenta")
         table.add_column("文件名")
         table.add_column("状态")
         table.add_row(name, '[bold red]未找到文献[/bold red]')
         console.print(table)
-        return None
+        urls[doi] = None
+        return False
 
 
 def lbggetdllink(name, doi):
@@ -129,40 +134,48 @@ def lbggetdllink(name, doi):
         div = afterhtml.find('a')
         pdflink = div.get('href')
         print('下载链接：' + pdflink)
-        return pdflink
+        urls[doi] = pdflink
+        return True
     except (Exception):
         table = Table(show_header=True, header_style="bold magenta")
         table.add_column("文件名")
         table.add_column("状态")
         table.add_row(name, '[bold red]未找到文献[/bold red]')
         console.print(table)
-        return None
+        urls[doi] = None
+        return False
 
 
-def continuousdl(name, doi, i):
+def continuousgetlink(name, doi, i):
     if (i + 1) % 3 == 1:
-        if downloadfile(name, doi, stwgetdllink(name, doi)) is False:
-            if downloadfile(name, doi, lbggetdllink(name, doi)) is False:
-                downloadfile(name, doi, ssegetdllink(name, doi))
+        if stwgetdllink(name, doi) is False:
+            if lbggetdllink(name, doi) is False:
+                ssegetdllink(name, doi)
     elif (i + 1) % 3 == 2:
-        if downloadfile(name, doi, lbggetdllink(name, doi)) is False:
-            if downloadfile(name, doi, ssegetdllink(name, doi)) is False:
-                downloadfile(name, doi, stwgetdllink(name, doi))
+        if lbggetdllink(name, doi) is False:
+            if ssegetdllink(name, doi) is False:
+                stwgetdllink(name, doi)
     else:
-        if downloadfile(name, doi, ssegetdllink(name, doi)) is False:
-            if downloadfile(name, doi, lbggetdllink(name, doi)) is False:
-                downloadfile(name, doi, stwgetdllink(name, doi))
+        if ssegetdllink(name, doi) is False:
+            if lbggetdllink(name, doi) is False:
+                stwgetdllink(name, doi)
 
 
 if __name__ == "__main__":
     init()
     console = Console()
-    print('请输入DOI')
-    doi = input('若为多个请用英文逗号分隔：')
+    print('特别感谢rich项目 https://github.com/willmcgugan/rich \n作者@evilbutcher\n\n若为多篇文献请用[bold red]英文逗号[/bold red]分隔')
+    doi = input('请输入DOI：')
+    print('\n开始下载...\n[bold yellow]说明[/bold yellow]：如果提示文献未找到，可暂时不用管，只是其中一个接口未找到文献，会自动更新接口获取下载地址')
     dois = list(doi.split(','))
+    print('\n输入的doi为：')
     print(dois)
+    urls = {}
     for index, value in enumerate(dois):
         filename = value.replace('/', '_') + '.pdf'
-        print('保存文件名：' + filename)
-        continuousdl(filename, value, index)
-    input('按任意键退出')
+        continuousgetlink(filename, value, index)
+    print('\n匹配下载链接[bold green]完成[/bold green]')
+    print(urls)
+    print('\n下载进度')
+    download(urls, './articles/')
+    input('下载完成，如遇到问题欢迎前往 https://github.com/evilbutcher/Python 提出issue，请按任意键退出')
--- README.md
+++ README.md
@@ -4,13 +4,14 @@
 一开始写了JavaScript版的[文献下载助手](https://github.com/evilbutcher/Code/tree/master/%E6%96%87%E7%8C%AE%E4%B8%8B%E8%BD%BD/%E6%96%87%E7%8C%AE%E4%B8%8B%E8%BD%BD%E5%8A%A9%E6%89%8B)，但这个只能在JSBox上运行，有一定的限制和门槛。
 时至今日，我终于捡起来Python，开始着手移植，一边移植一边学python...  
 ### Todo 
-图形界面 解析wos记录 进度条  
+解析wos记录 
 ### 现已支持
 1.根据doi进行文献下载和保存  
 2.下载异常判断  
 3.批量下载  
 4.自动交替请求下载  
 5.下载失败自动更换地址  
+6.进度条  
 
 ### 特别感谢：
 [@rich](https://github.com/willmcgugan/rich)
